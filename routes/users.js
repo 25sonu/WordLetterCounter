a3c569593b17b1b751d58936510d801a58fd09ab
25sonu <pratibhajena288@gmail.com>
2024-09-12 10:05:44 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+import { v4 as uuidv4} from 'uuid';
+
+const router = express.Router();
+
+// const users =[
+//     {
+//         first_name: 'john',
+//         last_name: 'doe',
+//         email: ' johndoe@example.com'
+//     },
+//     {
+//         first_name: 'alice',
+//         last_name: 'smith',
+//         email: 'alicesmith@example.com'
+//     },
+// ]
+
+const users= [];
+
+router.get('/', (req, res) => {
+    res.send(users);
+})
+
+router.post('/', (req,res) =>{
+    const user= req.body;
+
+    users.push({ ...user, id: uuidv4()});
+
+    res.send(` ${user.first_name} has been added to the database`)
+})
+
+export default router
